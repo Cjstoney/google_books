0115d1fc285fee321429d5764a4e3d92d33d5672
Camerone Stoney <Cameronestoney@yahoo.com>
2019-08-28 16:12:09 -0700

    WIP server file.  Need assistance

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes here
+// /api/books (get) - Should return all saved books as JSON.
+// /api/books (post) - Will be used to save a new book to the database.
+// /api/books/:id (delete) - Will be used to delete a book from the database by Mongo _id.
+// * (get) - Will load your single HTML page in client/build/index.html. Make sure you have this after all other routes are defined.
+
+get("/api/books", (req,res)=>{
+
+})
+
+
+post('api/books')
+
+delete('api/books')
+
+
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
