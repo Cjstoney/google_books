97cd8db05d16a146134d98631c71c64d21ffec4a
Camerone Stoney <Cameronestoney@yahoo.com>
2019-08-28 14:19:37 -0700

    searchPage component and SavedPage component added

---
+++ client/src/pages/Saved-page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeaderBar from './components/navbar'
+
+const SavedPage = () => {
+  return (
+    <div className="App">
+      <HeaderBar />
+    
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Search-page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeaderBar from './components/navbar'
+
+const SearchPage = ()=>{
+  return (
+    <div className="App">
+      <HeaderBar />
+    
+     
+    </div>
+  );
+}
+
+export default App;
